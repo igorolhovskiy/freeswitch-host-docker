0eeb19519ba4f99b0204fd746c970131e3169f28
Igor Olhovskiy <IgorOlhovskiy@gmail.com>
2020-03-11 10:06:35 +0200

    Events in lua are single-threaded

--- freeswitch/scripts/test-catch.lua
+++ freeswitch/scripts/test-catch.lua
@@ -1,10 +1,20 @@
-file = io.open("/usr/share/freeswitch/scripts/out.log", "a")
-io.output(file)
+local clock = os.clock
 
-if freeswitch then
-    freeswitch.consoleLog("info", "Catch event from test-catch.lua!")
-    io.write("Catch event from test-catch.lua to console!\n")
+function sleep(n)  -- seconds
+    local t0 = clock()
+    while clock() - t0 <= n do end
+  end
+
+if freeswitch then    
+    freeswitch.consoleLog("info", "Catch event from test-catch.lua! Sleeping for 10 sec")
+    sleep(10)
+    freeswitch.consoleLog("info", "Sleep ended! Testing CURL")
+    api = freeswitch.API()
+    response = api:execute("curl", "10.0.0.1 headers")
+    freeswitch.consoleLog("info", response)
 else
+    file = io.open("/usr/share/freeswitch/scripts/out.log", "a")
+    io.output(file)
     io.write("Catch event from test-catch.lua to file!\n")
+    io.close(file)
 end
-io.close(file)
